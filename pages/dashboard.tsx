7e4c455c66e1e347ff17841e416636487f43f4d4
takashi247 <takashi247@gmail.com>
2022-10-12 01:22:17 +0900

    complete lessens for frontend until #18 at Udemy

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,10 +3,14 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '../components/Layout'
+import { UserInfo } from '../components/UserInfo'
+import { useQueryClient } from '@tanstack/react-query' // to remove cached info re: login users when the user logouts
 
 const Dashboard = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
   const logout = async () => {
+    queryClient.removeQueries(['user'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
@@ -16,6 +20,7 @@ const Dashboard = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={logout}
       />
+      <UserInfo />
     </Layout>
   )
 }
